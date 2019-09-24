941274baa34718a485dffe746058290fe9d5484b
JJ Asghar <jjasghar@gmail.com>
2019-09-24 16:22:58 -0500

    Removed hanging data.
    Signed-off-by: JJ Asghar <awesome@ibm.com>

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const portNumber = process.argv[2] || 8000;
 const server = net.createServer((socket) => {
   let time = Math.floor(new Date() / 1000)
 
-  // Uncomment the following for a v2 that adds the full date: 2019-08-13 12:47
-  //let formattedDate = date.getFullYear() + "-" + month  + "-" + monthDate + " " + hours + ":" + minutes ;
   let email = time + "@asgharlabs.io"
 
   socket.write("email is: " + email);
