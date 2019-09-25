ad369274ef9ece90c9a4ee116954ebdd15c72d19
JJ Asghar <jjasghar@gmail.com>
2019-09-25 10:45:03 -0500

    i forgot ;

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const net = require('net');
 const portNumber = process.argv[2] || 8080;
 
 const server = net.createServer((socket) => {
-  let time = Math.floor(new Date() / 1000)
+  let time = Math.floor(new Date() / 1000);
 
-  let email = time + "@asgharlabs.io"
+  let email = time + "@asgharlabs.io";
 
   socket.write("email is: " + email);
   socket.end("\n");
